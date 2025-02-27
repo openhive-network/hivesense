789dffbcb9e1bf155a782c1500dd0fcf66a5b808
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-02-27 13:32:02 +0100

    processing works, but problem with breaking with ctrl+c

--- scripts/process_blocks.sh
+++ scripts/process_blocks.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -x
 set -e
 set -o pipefail
 # Script reponsible for execution of all actions required to finish configuration of the database holding a HAF database to work correctly with hivemind.
@@ -67,12 +67,37 @@ done
 POSTGRES_ACCESS=${POSTGRES_URL:-"postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT/haf_block_log?application_name=hivesense_block_processing"}
 
 process_blocks() {
-    local n_blocks="${1:-null}"
+    local n_blocks="${2:-null}"
+    local worker=${1}
     log_file="hivesense_sync.log"
     # record the startup time for use in health checks
     date -uIseconds > /tmp/block_processing_startup_time.txt
 
-    psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -v HIVESENSE_SCHEMA="${HIVESENSE_SCHEMA}" -c "\timing" -c "SET SEARCH_PATH TO ${HIVESENSE_SCHEMA};" -c "CALL ${HIVESENSE_SCHEMA}.main('${HIVESENSE_SCHEMA}', $n_blocks);" 2>&1 | tee -i $log_file
+    psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -v HIVESENSE_SCHEMA="${HIVESENSE_SCHEMA}" -c "\timing" -c "SET SEARCH_PATH TO ${HIVESENSE_SCHEMA};" -c "CALL ${HIVESENSE_SCHEMA}.main('${HIVESENSE_SCHEMA}', ${worker}, $n_blocks );" 2>&1 | tee -i $log_file
 }
 
-process_blocks "$PROCESS_BLOCK_LIMIT"
+# gen number of workers
+NUMBER_OF_WORKERS="$(psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -t -c "SELECT parallel_workers FROM ${HIVESENSE_SCHEMA}.hivesense_app_status" | xargs)";
+
+pids=()
+
+i=1
+
+while [ "$i" -le "$NUMBER_OF_WORKERS" ]; do
+    process_blocks "$i" "$PROCESS_BLOCK_LIMIT" &
+    pids+=($!)
+    i=$((i + 1))
+done
+
+terminate_jobs() {
+    echo "Breaking HiveSense workers ${pids[@]}";
+    psql "$POSTGRES_ACCESS" -v "ON_ERROR_STOP=on" -t -c "SET SEARCH_PATH TO ${HIVESENSE_SCHEMA};SELECT ${HIVESENSE_SCHEMA}.stopProcessing()";
+    wait "${pids[@]}"
+    echo "Stopped HiveSense workers ${pids[@]}";
+}
+
+trap 'terminate_jobs' INT TERM
+trap 'terminate_jobs' EXIT
+
+wait
+echo "Stopped HiveSense workers  2 ${pids[@]}";
