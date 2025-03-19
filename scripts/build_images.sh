59af4d3720f94a69d69ac5f3f82635448d83084d
Marcin Ickiewicz <mickiewicz@syncad.com>
2025-03-19 14:15:04 +0100

    refactor build instance

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -5,7 +5,37 @@ if [ -z "$GIT_COMMIT_SHA" ]; then
   GIT_COMMIT_SHA="[unknown]"
 fi
 
-TAG=${1:-${GIT_COMMIT_SHA:0:8}}
+print_help () {
+cat <<-EOF
+  Usage: $0 <image> <local-directory> [OPTION[=VALUE]]...
+
+  Exports data from a Docker image to a local directory
+  OPTIONS:
+    --image-path=PATH         Path inside the Docker image that is to be exported (default: /home/hived/bin/).
+    --help,-h,-?              Display this help screen and exit
+EOF
+}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --tag=*)
+        TAG="${1#*=}"
+        ;;
+    --help|-h|-\?)
+        print_help
+        exit 0
+        ;;
+    -*)
+        echo "ERROR: '$1' is not a valid option"
+        exit 1
+        ;;
+    *)
+        echo "ERROR: '$1' is not a valid positional argument"
+    esac
+    shift
+done
+
+TAG=${TAG:-${GIT_COMMIT_SHA:0:8}}
 
 if [ -z "$TAG" ]; then
   echo "No tag, please pass it at first argument" >&2
